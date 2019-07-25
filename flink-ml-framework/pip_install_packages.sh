ce246af24566a05222fd4e16a65fb1408ca5cfd5
tyw <291363040@qq.com>
2019-07-25 11:46:40 +0800

    modify pip.install.option

--- flink-ml-framework/pip_install_packages.sh
+++ flink-ml-framework/pip_install_packages.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
-
-
 #mapfile array <user.properties
-
 array=()
-for line in `cat ../user.properties`
+while read line|| [[ -n ${line} ]]
 do
     array+=("$line")
-done
+done<../user.properties
 
 len=${#array[*]}
 PACKAGES=""
-for i in $(seq 2 1 ${len})
+for i in $(seq 3 1 ${len})
 do
     PACKAGES="$PACKAGES ${array[i]}"
 done
 
 pip_version=${array[1]}
+option=${array[2]}
 OLD_IFS="$IFS"
 IFS="="
 pip_array=($pip_version)
+option_array=($option)
 IFS="$OLD_IFS"
 pipV=${pip_array[1]}
+option=${option_array[1]}
 
-${pipV} install --user ${PACKAGES}
+${pipV} install ${option} ${PACKAGES}
