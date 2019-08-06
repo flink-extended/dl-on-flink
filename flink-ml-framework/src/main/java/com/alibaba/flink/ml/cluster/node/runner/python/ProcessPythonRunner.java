bc35397ae8ec7515c8bbc3dc1f0b62f789a51268
wuchao.cwc <wuchao.cwc@alibaba-inc.com>
2019-08-06 15:00:15 +0800

    [hotfix] set venv site-packages to PYTHONPATH

--- flink-ml-framework/src/main/java/com/alibaba/flink/ml/cluster/node/runner/python/ProcessPythonRunner.java
+++ flink-ml-framework/src/main/java/com/alibaba/flink/ml/cluster/node/runner/python/ProcessPythonRunner.java
@@ -136,7 +136,8 @@ public class ProcessPythonRunner extends AbstractScriptRunner {
 		StringBuilder pldPath = new StringBuilder();
 		String workerDir = mlContext.getProperties().get(MLConstants.WORK_DIR);
 		String codePath = mlContext.getProperties().getOrDefault(MLConstants.CODE_DIR, workerDir);
-		String finalPythonPath = mlContext.getProperties().getOrDefault(MLConstants.PYTHONPATH_ENV, "")
+		String finalPythonPath = mlContext.getProperties()
+				.getOrDefault(MLConstants.ENV_PROPERTY_PREFIX + MLConstants.PYTHONPATH_ENV, "")
 				+ ":" + codePath;
 		mlContext.putEnvProperty(MLConstants.PYTHONPATH_ENV, finalPythonPath);
 
