85d21fd626c019d80c129d0bc89a6427814c5172
tyw <291363040@qq.com>
2019-08-12 20:38:26 +0800

    table pythonInterfaces which are for TF running

--- flink-ml-tensorflow/python/flink_ml_tensorflow/tensorflow_on_flink_table.py
+++ flink-ml-tensorflow/python/flink_ml_tensorflow/tensorflow_on_flink_table.py
@@ -13,63 +13,66 @@
 # limitations under the License.
 # =============================================================================
 
+# Based on Pyflink,which means all these interfaces are based on Flink1.9
+
 from pyflink.java_gateway import get_gateway
-from tensorflow_on_flink.pyflink_tensorflow import TFConfig
-from pyflink.environment import StreamExecutionEnvironment
-from pyflink.table.table_environment import TableEnvironment
-from pyflink.table.table_environment import Table
+from tensorflow_TFConfig import TFConfig
+from pyflink.datastream.stream_execution_environment import StreamExecutionEnvironment
+from pyflink.table import StreamTableEnvironment
+from pyflink.table.table import Table
 
 
-def inference(num_worker, num_ps=0, func=None, properties=None, env_path=None, zk_conn=None, zk_base_path=None,
+def inference(num_worker, num_ps=0, python_file=None, func=None, properties=None, env_path=None, zk_conn=None, zk_base_path=None,
               stream_env=None, table_env=None, input_table=None, output_schema=None):
     """
     Tensorflow inference for Table
-    :param zk_conn: The Zookeeper connection string
-    :param zk_base_path: The Zookeeper base path
     :param num_worker: Number of workers
     :param num_ps: Number of PS
+    :param python_file: The python file which is going to be run
     :param func: The user-defined function that runs TF inference. If it's None, inference is run via Java API.
     :param properties: User-defined properties
     :param env_path: Path to the virtual env
     :param stream_env: The StreamExecutionEnvironment. If it's None, this method will create one and execute the job
                        at the end. Otherwise, caller is responsible to trigger the job execution
     :param table_env: The TableEnvironment
+    :param zk_conn: The Zookeeper connection string
+    :param zk_base_path: The Zookeeper base path
     :param input_table: The input Table
     :param output_schema: The TableSchema of the output Table. If it's None, a dummy sink will be added to the output
                           Table. Otherwise, caller is responsible to add sink before executing the job.
     :return: The output Table
     """
-    tf_config = TFConfig(num_worker, num_ps, func, properties, env_path, zk_conn, zk_base_path)
-    execute = stream_env is None
+    tf_config = TFConfig(num_worker, num_ps, python_file, func, properties, env_path, zk_conn, zk_base_path)
     if stream_env is None:
         stream_env = StreamExecutionEnvironment.get_execution_environment()
     if table_env is None:
-        table_env = TableEnvironment.get_table_environment(stream_env)
+        table_env = StreamTableEnvironment.create(stream_env)
     if input_table is not None:
         input_table = input_table._java_table
     if output_schema is not None:
         output_schema = output_schema._j_table_schema
-    output_table = get_gateway().jvm.com.alibaba.flink.tensorflow.client.TFUtils.inference(stream_env._j_env,
+    output_table = get_gateway().jvm.com.alibaba.flink.ml.tensorflow.client.TFUtils.inference(
+                                                                                           stream_env._j_stream_execution_environment,
                                                                                            table_env._j_tenv,
                                                                                            input_table,
                                                                                            tf_config.java_config(),
                                                                                            output_schema)
-    if execute:
-        table_env.execute()
-    return Table(java_table=output_table)
+
+    table_env.execute(job_name="table inference")
+    return Table(output_table)
 
 
-def train(num_worker, num_ps, func, properties=None, env_path=None, zk_conn=None, zk_base_path=None,
+def train(num_worker, num_ps, python_file, func, properties=None, env_path=None, zk_conn=None, zk_base_path=None,
           stream_env=None, table_env=None, input_table=None, output_schema=None):
     """
     Tensorflow training for Table
-    :param zk_conn: The Zookeeper connection string
-    :param zk_base_path: The Zookeeper base path
     :param num_worker: Number of workers
     :param num_ps: Number of PS
     :param func: The user-defined function that runs TF training
     :param properties: User-defined properties
     :param env_path: Path to the virtual env
+    :param zk_conn: The Zookeeper connection string  //zk location
+    :param zk_base_path:    //pei zhi wen jian dizhi
     :param stream_env: The StreamExecutionEnvironment. If it's None, this method will create one and execute the job
                        at the end. Otherwise, caller is responsible to trigger the job execution
     :param table_env: The TableEnvironment
@@ -78,20 +81,20 @@ def train(num_worker, num_ps, func, properties=None, env_path=None, zk_conn=None
                           Table. Otherwise, caller is responsible to add sink before executing the job.
     :return: The output Table
     """
-    tf_config = TFConfig(num_worker, num_ps, func, properties, env_path, zk_conn, zk_base_path)
-    execute = stream_env is None
+    tf_config = TFConfig(num_worker, num_ps, python_file, func, properties, env_path, zk_conn, zk_base_path)
     if stream_env is None:
         stream_env = StreamExecutionEnvironment.get_execution_environment()
     if table_env is None:
-        table_env = TableEnvironment.get_table_environment(stream_env)
+        table_env = StreamTableEnvironment.create(stream_env)
     if input_table is not None:
         input_table = input_table._java_table
     if output_schema is not None:
         output_schema = output_schema._j_table_schema
-    output_table = get_gateway().jvm.com.alibaba.flink.tensorflow.client.TFUtils.train(stream_env._j_env,
+    tf_c = tf_config.java_config()
+    output_table = get_gateway().jvm.com.alibaba.flink.ml.tensorflow.client.TFUtils.train(
+                                                                                       stream_env._j_stream_execution_environment,
                                                                                        table_env._j_tenv, input_table,
-                                                                                       tf_config.java_config(),
+                                                                                       tf_c,
                                                                                        output_schema)
-    if execute:
-        table_env.execute()
-    return Table(java_table=output_table)
+    table_env.execute(job_name="table train")
+    return Table(output_table)
