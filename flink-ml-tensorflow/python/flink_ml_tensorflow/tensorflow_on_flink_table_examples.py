c3d5088cec419923a2eb73c53e89263476d63d5d
tyw <291363040@qq.com>
2019-08-16 15:59:54 +0800

    add datastream python interfaces and some examples

--- flink-ml-tensorflow/python/flink_ml_tensorflow/tensorflow_on_flink_table_examples.py
+++ flink-ml-tensorflow/python/flink_ml_tensorflow/tensorflow_on_flink_table_examples.py
@@ -1,9 +1,23 @@
-import os
+# Copyright 2019 The flink-ai-extended Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
 
+import os
 from pyflink.datastream.stream_execution_environment import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment
 from pyflink.table.table import TableSchema
-from tensorflow_on_flink_table import train,inference
+from tensorflow_on_flink_table import train, inference
 from tensorflow_on_flink_tfconf import TFCONSTANS
 from tensorflow_on_flink_mlconf import MLCONSTANTS
 from pyflink.table.sources import CsvTableSource
@@ -22,11 +36,15 @@ class tableTest(object):
         func = "map_func"
         property = None
         env_path = None
+        zk_conn = None
+        zk_base_path = None
         input_tb = None
         output_schema = None
 
-        train(work_num,ps_num,python_file,func,property,env_path,None,None,stream_env,table_env,input_tb,output_schema)
-        # inference(work_num, ps_num, python_file, func, property, env_path, None, None, stream_env, table_env, input_tb,output_schema)
+        train(work_num, ps_num, python_file, func, property, env_path, zk_conn, zk_base_path, stream_env, table_env,
+              input_tb, output_schema)
+        # inference(work_num, ps_num, python_file, func, property, env_path, zk_conn, zk_base_path, stream_env, table_env,
+        #           input_tb, output_schema)
 
     @staticmethod
     def addTrainChiefAloneTable():
@@ -39,11 +57,15 @@ class tableTest(object):
         property = {}
         property[TFCONSTANS.TF_IS_CHIEF_ALONE] = "ture"
         env_path = None
+        zk_conn = None
+        zk_base_path = None
         input_tb = None
         output_schema = None
 
-        train(work_num,ps_num,python_file,func,property,env_path,None,None,stream_env,table_env,input_tb,output_schema)
-        # inference(work_num, ps_num, python_file, func, property, env_path, None, None, stream_env, table_env, input_tb,output_schema)
+        train(work_num, ps_num, python_file, func, property, env_path, zk_conn, zk_base_path, stream_env, table_env,
+              input_tb, output_schema)
+        # inference(work_num, ps_num, python_file, func, property, env_path, zk_conn, zk_base_path, stream_env, table_env,
+        #           input_tb, output_schema)
 
     @staticmethod
     def inputOutputTable():
@@ -55,6 +77,8 @@ class tableTest(object):
         property = {}
         func = "map_func"
         env_path = None
+        zk_conn = None
+        zk_base_path = None
         property[MLCONSTANTS.ENCODING_CLASS] = "com.alibaba.flink.ml.operator.coding.RowCSVCoding"
         property[MLCONSTANTS.DECODING_CLASS] = "com.alibaba.flink.ml.operator.coding.RowCSVCoding"
         inputSb = "INT_32" + "," + "INT_64" + "," + "FLOAT_32" + "," + "FLOAT_64" + "," + "STRING"
@@ -62,26 +86,25 @@ class tableTest(object):
         property["SYS:csv_decode_types"] = inputSb
         source_file = os.getcwd() + "/../../src/test/resources/input.csv"
         table_source = CsvTableSource(source_file,
-                                      ["a","b","c","d","e"],
+                                      ["a", "b", "c", "d", "e"],
                                       [DataTypes.INT(),
                                        DataTypes.INT(),
                                        DataTypes.FLOAT(),
                                        DataTypes.DOUBLE(),
                                        DataTypes.STRING()])
-        table_env.register_table_source("source",table_source)
+        table_env.register_table_source("source", table_source)
         input_tb = table_env.scan("source")
-        output_schema = TableSchema(["a","b","c","d","e"],
+        output_schema = TableSchema(["a", "b", "c", "d", "e"],
                                     [DataTypes.INT(),
                                      DataTypes.INT(),
                                      DataTypes.FLOAT(),
                                      DataTypes.DOUBLE(),
                                      DataTypes.STRING()]
                                     )
-        train(work_num,ps_num,python_file,func,property,env_path,None,None,stream_env,table_env,input_tb,output_schema)
-        # inference(work_num, ps_num, python_file, func, property, env_path, None, None, stream_env, table_env, input_tb,output_schema)
-
-
-
+        train(work_num, ps_num, python_file, func, property, env_path, zk_conn, zk_base_path, stream_env, table_env,
+              input_tb, output_schema)
+        # inference(work_num, ps_num, python_file, func, property, env_path, zk_conn, zk_base_path, stream_env, table_env,
+        #           input_tb, output_schema)
 
     @staticmethod
     def testWorkerZeroFinish():
@@ -93,11 +116,15 @@ class tableTest(object):
         func = "map_func"
         property = None
         env_path = None
+        zk_conn = None
+        zk_base_path = None
         input_tb = None
         output_schema = None
 
-        train(work_num,ps_num,python_file,func,property,env_path,None,None,stream_env,table_env,input_tb,output_schema)
-        # inference(work_num, ps_num, python_file, func, property, env_path, None, None, stream_env, table_env, input_tb,output_schema)
+        train(work_num, ps_num, python_file, func, property, env_path, zk_conn, zk_base_path, stream_env, table_env,
+              input_tb, output_schema)
+        # inference(work_num, ps_num, python_file, func, property, env_path, zk_conn, zk_base_path, stream_env, table_env,
+        #           input_tb, output_schema)
 
 
 if __name__ == '__main__':
