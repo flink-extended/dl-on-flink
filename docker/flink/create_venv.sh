5fada94fef08365048628182483a431edbb57169
sxnan <suxuannan95@gmail.com>
2020-07-06 15:09:34 +0800

    fix tensorflow it test - use hadoop 2.8 - use python3 - update mnist data urls

--- docker/flink/create_venv.sh
+++ docker/flink/create_venv.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-virtualenv /opt/work_home/temp/test/tfenv
+virtualenv -p python3 /opt/work_home/temp/test/tfenv
 cd /opt/work_home/temp/test
 . tfenv/bin/activate
 array=()
@@ -22,14 +22,15 @@ done
 TENSORFLOW=${array[12]}
 TORCH=${array[13]}
 pip install ${PACKAGES}
-pip install -i https://pypi.doubanio.com/simple ${TENSORFLOW}
+pip install ${TENSORFLOW}
 #pip install grpcio six numpy wheel mock keras_applications keras_preprocessing
 #pip install -i https://pypi.doubanio.com/simple tensorflow==1.11.0
 #pip install -i https://pypi.doubanio.com/simple torch torchvision
 # pip install torch==1.1.0
 # download torch slow so use aliyun address
 # pip install http://etaose.oss-cn-hangzhou-zmf.aliyuncs.com/test/flink-ml/package/torch-1.0.1.post2-cp27-cp27mu-manylinux1_x86_64.whl
-pip install http://etaose.oss-cn-hangzhou-zmf.aliyuncs.com/test/flink-ml/package/torch-1.1.0-cp27-cp27mu-manylinux1_x86_64.whl
+#pip install http://etaose.oss-cn-hangzhou-zmf.aliyuncs.com/test/flink-ml/package/torch-1.1.0-cp27-cp27mu-manylinux1_x86_64.whl
+pip install torch==1.1.0
 pip install ${TORCH}
 #pip install torchvision==0.3.0
 cd /opt/work_home/flink-ml-framework/python
@@ -38,7 +39,7 @@ cd /opt/work_home/flink-ml-tensorflow/python
 pip install .
 cd /opt/work_home/temp/test
 deactivate
-touch /opt/work_home/temp/test/tfenv/lib/python2.7/site-packages/google/__init__.py
+touch /opt/work_home/temp/test/tfenv/lib/python3.5/site-packages/google/__init__.py
 
 cd /opt/work_home/temp/test/
 zip -ryq tfenv.zip tfenv
