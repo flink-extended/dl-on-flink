124e6951b46bbe907c1afd5efaa5a747d7ed8ae0
sxnan <suxuannan95@gmail.com>
2020-07-06 15:09:34 +0800

    fix tensorflow it test - use hadoop 2.8 - use python3 - update mnist data urls

--- docker/build_cluster/download_mnist_data.sh
+++ docker/build_cluster/download_mnist_data.sh
@@ -6,7 +6,7 @@ set -e
 
 dataDir=${projectPath}/flink-ml-examples/target/data
 mkdir -p  ${dataDir}
-wget http://etaose.oss-cn-hangzhou-zmf.aliyuncs.com/test/flink-ml/data/t10k-images-idx3-ubyte.gz -O ${dataDir}/t10k-images-idx3-ubyte.gz
-wget http://etaose.oss-cn-hangzhou-zmf.aliyuncs.com/test/flink-ml/data/t10k-labels-idx1-ubyte.gz -O ${dataDir}/t10k-labels-idx1-ubyte.gz
-wget http://etaose.oss-cn-hangzhou-zmf.aliyuncs.com/test/flink-ml/data/train-images-idx3-ubyte.gz -O ${dataDir}/train-images-idx3-ubyte.gz
-wget http://etaose.oss-cn-hangzhou-zmf.aliyuncs.com/test/flink-ml/data/train-labels-idx1-ubyte.gz -O ${dataDir}/train-labels-idx1-ubyte.gz
+wget https://raw.githubusercontent.com/wuchaochen/testdata/master/mnist/t10k-images-idx3-ubyte.gz -O ${dataDir}/t10k-images-idx3-ubyte.gz
+wget https://raw.githubusercontent.com/wuchaochen/testdata/master/mnist/t10k-labels-idx1-ubyte.gz -O ${dataDir}/t10k-labels-idx1-ubyte.gz
+wget https://raw.githubusercontent.com/wuchaochen/testdata/master/mnist/train-images-idx3-ubyte.gz -O ${dataDir}/train-images-idx3-ubyte.gz
+wget https://raw.githubusercontent.com/wuchaochen/testdata/master/mnist/train-labels-idx1-ubyte.gz -O ${dataDir}/train-labels-idx1-ubyte.gz
