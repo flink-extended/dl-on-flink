4e0bcdfaf56c7d62e985db419a0d1125d59a20dc
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-03-31 17:38:29 +0800

    quickstart interface change

--- flink-ai-flow/examples/quickstart_example/python_codes/airflow_dag_example.py
+++ flink-ai-flow/examples/quickstart_example/python_codes/airflow_dag_example.py
@@ -39,7 +39,7 @@ def run_workflow():
     af.set_project_config_file(project_path + '/project.yaml')
     # the airflow scheduler do not support waiting until the execution finished
     # so we just submit the workflow and exit
-    af.run(project_path, dag_id='airflow_dag_example', scheduler_type=SchedulerType.AIRFLOW)
+    af.deploy_to_airflow(project_path, dag_id='airflow_dag_example')
 
 
 if __name__ == '__main__':
