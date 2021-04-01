5ecfd09d553afd5fd1d96f877656979f9e39ce13
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-04-01 14:31:45 +0800

    fix missing serialized dag

--- flink-ai-flow/lib/airflow/airflow/cli/commands/task_command.py
+++ flink-ai-flow/lib/airflow/airflow/cli/commands/task_command.py
@@ -213,7 +213,10 @@ def task_run(args, dag=None):
         print(f'Loading pickle id: {args.pickle}')
         dag = get_dag_by_pickle(args.pickle)
     elif not dag:
-        dag = get_dag(args.subdir, args.dag_id)
+        try:
+            dag = get_dag(args.subdir, args.dag_id)
+        except AirflowException:
+            dag = get_dag_from_db(args.subdir, args.dag_id)
     else:
         # Use DAG from parameter
         pass
