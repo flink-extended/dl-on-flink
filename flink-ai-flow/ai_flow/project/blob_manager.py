8a0a989f0d10970b9ffdbf70686b0505cb2f32f1
wuchaochen <wuchao_chen@qq.com>
2021-05-07 12:13:00 +0800

    oss blob manager can run.

--- flink-ai-flow/ai_flow/project/blob_manager.py
+++ flink-ai-flow/ai_flow/project/blob_manager.py
@@ -24,6 +24,7 @@ from typing import Text, Dict, Any
 from pathlib import Path
 import oss2
 
+from ai_flow.common.module_load import import_string
 from ai_flow.util.file_util.zip_file_util import make_dir_zipfile
 
 
@@ -66,13 +67,14 @@ class BlobManager(ABC):
 
 class LocalBlobManager(BlobManager):
     def __init__(self, config: Dict[str, Any]):
-        self._repo = config.get('repository', None)
+        self._local_repo = config.get('local_repository', None)
+        self._remote_repo = config.get('remote_repository', None)
 
     def upload_blob(self, workflow_id: Text, prj_pkg_path: Text) -> Text:
-        if self._repo is not None:
+        if self._remote_repo is not None:
             with tempfile.TemporaryDirectory() as temp_dir:
                 zip_file_name = 'workflow_{}_project.zip'.format(workflow_id)
-                upload_file_path = Path('/tmp/{}'.format(zip_file_name))
+                upload_file_path = Path('{}/{}'.format(self._remote_repo, zip_file_name))
                 if os.path.exists(upload_file_path):
                     os.remove(upload_file_path)
                 temp_dir_path = Path(temp_dir)
@@ -84,11 +86,12 @@ class LocalBlobManager(BlobManager):
             return prj_pkg_path
 
     def download_blob(self, workflow_id, remote_path: Text, local_path: Text = None) -> Text:
-        if self._repo is not None:
+        if local_path is not None or self._local_repo is not None:
+            repo_path = local_path if local_path is not None else self._local_repo
             local_zip_file_name = 'workflow_{}_project'.format(workflow_id)
             with zipfile.ZipFile(remote_path, 'r') as zip_ref:
                 top_dir = os.path.split(zip_ref.namelist()[0])[0]
-                extract_path = str(Path(self._repo) / local_zip_file_name)
+                extract_path = str(Path(repo_path) / local_zip_file_name)
                 downloaded_local_path = Path(extract_path) / top_dir
 
                 if os.path.exists(str(downloaded_local_path)):
@@ -114,7 +117,8 @@ class OssBlobManager(BlobManager):
         bucket_name = config.get('blob_server.bucket', None)
         auth = oss2.Auth(ack_id, ack_secret)
         self.bucket = oss2.Bucket(auth, endpoint, bucket_name)
-        self._repo = config.get('repository', None)
+        self.repo_name = config.get('blob_server.repo_name', '')
+        self._local_repo = config.get('local_repository', None)
 
     def upload_blob(self, workflow_id: Text, prj_pkg_path: Text) -> Text:
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -122,17 +126,19 @@ class OssBlobManager(BlobManager):
             temp_dir_path = Path(temp_dir)
             zip_file_path = temp_dir_path / zip_file_name
             make_dir_zipfile(prj_pkg_path, zip_file_path)
-            object_key = 'ai-flow-k8s/' + zip_file_name
+            object_key = self.repo_name + '/' + zip_file_name
             self.bucket.put_object_from_file(key=object_key, filename=str(zip_file_path))
         return object_key
 
     def download_blob(self, workflow_id, remote_path: Text, local_path: Text = None) -> Text:
         local_zip_file_name = 'workflow_{}_project'.format(workflow_id)
         oss_object_key = remote_path
-        if local_path is None:
-            tmp_dir = Path(tempfile.gettempdir())
-        else:
+        if local_path is not None:
             tmp_dir = Path(local_path)
+        elif self._local_repo is not None:
+            tmp_dir = Path(self._local_repo)
+        else:
+            tmp_dir = Path(tempfile.gettempdir())
         local_zip_file_path = str(tmp_dir / local_zip_file_name) + '.zip'
         self.bucket.get_object_to_file(oss_object_key, filename=local_zip_file_path)
         with zipfile.ZipFile(local_zip_file_path, 'r') as zip_ref:
@@ -152,4 +158,6 @@ class BlobManagerFactory:
             return blob_manager
         elif blob_server_type == 'oss':
             return OssBlobManager(config)
-        return blob_manager
+        else:
+            class_object = import_string(blob_server_type)
+            return class_object(config)
