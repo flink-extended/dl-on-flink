c019d22eca8eab2e418079681d94a9c625de484a
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-05-25 16:18:49 +0800

    [Flink AI Flow][Bug] EventBasedScheduler cannot send heartbeat

--- flink-ai-flow/lib/airflow/airflow/cli/cli_parser.py
+++ flink-ai-flow/lib/airflow/airflow/cli/cli_parser.py
@@ -1437,6 +1437,7 @@ airflow_commands: List[CLICommand] = [
             ARG_STDOUT,
             ARG_STDERR,
             ARG_LOG_FILE,
+            ARG_SERVER_URI,
         ),
         epilog=(
             'Signals:\n'
