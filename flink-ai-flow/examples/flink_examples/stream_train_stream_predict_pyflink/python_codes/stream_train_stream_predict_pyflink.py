a14bfd2223b789c699b99becaca792270124768e
bgeng777 <gengbiao.gb@alibaba-inc.com>
2021-06-24 22:48:21 +0800

    add pyflink example

---
+++ flink-ai-flow/examples/flink_examples/stream_train_stream_predict_pyflink/python_codes/stream_train_stream_predict_pyflink.py
@@ -0,0 +1,122 @@
+from ai_flow import ExecutionMode, ExampleSupportType, BaseJobConfig, PythonObjectExecutor, ModelType, \
+    ArtifactMeta
+from ai_flow.common.scheduler_type import SchedulerType
+
+from ai_flow.model_center.entity.model_version_stage import ModelVersionEventType
+from flink_ai_flow import FlinkPythonExecutor
+from flink_ai_flow import LocalFlinkJobConfig
+from stream_train_stream_predict_pyflink_executor import *
+
+EXAMPLE_URI = os.path.abspath(os.path.join(__file__, "../../../..")) + '/example_data/iris_{}.csv'
+
+
+def get_project_path():
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def run_pyflink_project(project_root_path):
+    af.set_project_config_file(get_project_path() + '/project.yaml')
+    project_name = af.project_config().get_project_name()
+    artifact_prefix = project_name + "."
+    with af.global_config_file(config_path=get_project_path() + '/resources/workflow_config.yaml'):
+        with af.config('train_job'):
+            # Training of model
+            # Register metadata raw training data(example) and read example(i.e. training dataset)
+            train_example = af.register_example(name='train_example',
+                                                support_type=ExampleSupportType.EXAMPLE_STREAM,
+                                                stream_uri=EXAMPLE_URI.format('train'),
+                                                data_format='csv')
+            train_model = af.register_model(model_name='iris_model',
+                                            model_type=ModelType.SAVED_MODEL,
+                                            model_desc='A KNN model')
+            train_read_example_channel = af.read_example(example_info=train_example,
+                                                         executor=PythonObjectExecutor(
+                                                             python_object=ReadTrainExample.LoadTrainExample()))
+            train_channel = af.train(input_data_list=[train_read_example_channel],
+                                     executor=PythonObjectExecutor(python_object=TrainModel()),
+                                     model_info=train_model)
+        with af.config('eval_job'):
+            # Evaluation of model
+            evaluate_example = af.register_example(name='evaluate_example',
+                                                   support_type=ExampleSupportType.EXAMPLE_STREAM,
+                                                   data_format='csv',
+                                                   batch_uri=EXAMPLE_URI.format('test'),
+                                                   stream_uri=EXAMPLE_URI.format('test'))
+            evaluate_example_channel = af.read_example(example_info=evaluate_example,
+                                                       executor=PythonObjectExecutor(python_object=TestExampleReader()))
+            evaluate_result = get_file_dir(__file__) + '/evaluate_result'
+            if os.path.exists(evaluate_result):
+                os.remove(evaluate_result)
+            evaluate_artifact: ArtifactMeta = af.register_artifact(name='evaluate_artifact',
+                                                                   batch_uri=evaluate_result,
+                                                                   stream_uri=evaluate_result)
+            evaluate_channel = af.evaluate(input_data_list=[evaluate_example_channel], model_info=train_model,
+                                           executor=PythonObjectExecutor(python_object=EvaluateModel()))
+
+        with af.config('validate_job'):
+            # Validation of model
+            # Read validation dataset and validate model before it is used to predict
+
+            validate_example = af.register_example(name='validate_example',
+                                                   support_type=ExampleSupportType.EXAMPLE_STREAM,
+                                                   data_format='csv',
+                                                   batch_uri=EXAMPLE_URI.format('test'),
+                                                   stream_uri=EXAMPLE_URI.format('test'))
+            validate_example_channel = af.read_example(example_info=validate_example,
+                                                       executor=PythonObjectExecutor(python_object=TestExampleReader()))
+            validate_result = get_file_dir(__file__) + '/validate_result'
+            if os.path.exists(validate_result):
+                os.remove(validate_result)
+            validate_artifact: ArtifactMeta = af.register_artifact(name='validate_artifact',
+                                                                   batch_uri=validate_result,
+                                                                   stream_uri=validate_result)
+            validate_channel = af.model_validate(input_data_list=[validate_example_channel], model_info=train_model,
+                                                 executor=PythonObjectExecutor(python_object=ValidateModel()))
+
+        with af.config('predict_job') as pyflink_job_config:
+            pyflink_job_config.set_table_env_create_func(StreamTableEnvCreator())
+            # Prediction(Inference)
+            predict_example = af.register_example(name='predict_example',
+                                                  support_type=ExampleSupportType.EXAMPLE_BATCH,
+                                                  batch_uri=EXAMPLE_URI.format('test'),
+                                                  stream_uri=EXAMPLE_URI.format('test'),
+                                                  data_format='csv')
+            predict_read_example = af.read_example(example_info=predict_example,
+                                                   executor=FlinkPythonExecutor(python_object=Source()))
+            predict_channel = af.predict(input_data_list=[predict_read_example],
+                                         model_info=train_model,
+                                         executor=FlinkPythonExecutor(python_object=Transformer()))
+
+            write_example = af.register_example(name='write_example',
+                                                support_type=ExampleSupportType.EXAMPLE_BATCH,
+                                                batch_uri=get_file_dir(
+                                                    __file__) + '/predict_model.csv',
+                                                stream_uri=get_file_dir(
+                                                    __file__) + '/predict_model.csv',
+                                                data_format='csv')
+            af.write_example(input_data=predict_channel,
+                             example_info=write_example,
+                             executor=FlinkPythonExecutor(python_object=Sink()))
+
+        af.model_version_control_dependency(src=evaluate_channel,
+                                            model_version_event_type=ModelVersionEventType.MODEL_GENERATED,
+                                            dependency=train_channel, model_name=train_model.name)
+
+        af.model_version_control_dependency(src=validate_channel,
+                                            model_version_event_type=ModelVersionEventType.MODEL_GENERATED,
+                                            dependency=train_channel, model_name=train_model.name)
+
+        af.model_version_control_dependency(src=predict_channel,
+                                            model_version_event_type=ModelVersionEventType.MODEL_DEPLOYED,
+                                            dependency=validate_channel, model_name=train_model.name)
+        # Run workflow
+        project_dag = project_name
+        af.deploy_to_airflow(project_root_path, dag_id=project_dag)
+        af.run(project_path=project_root_path,
+               dag_id=project_dag,
+               scheduler_type=SchedulerType.AIRFLOW)
+
+
+if __name__ == '__main__':
+    project_path = os.path.dirname(get_file_dir(__file__))
+    run_pyflink_project(project_path)
---
+++ flink-ai-flow/examples/flink_examples/stream_train_stream_predict_pyflink/python_codes/stream_train_stream_predict_pyflink_executor.py
@@ -0,0 +1,228 @@
+import os
+from ai_flow.util.time_utils import generate_time_str
+from joblib import dump, load
+from time import sleep
+import time
+from ai_flow.util.path_util import get_file_dir
+from ai_flow import ExampleMeta, ModelMeta, ModelVersionStage
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import Table, DataTypes, ScalarFunction, CsvTableSink, WriteMode, \
+    StreamTableEnvironment, EnvironmentSettings
+from pyflink.table.descriptors import FileSystem, OldCsv, Schema
+from pyflink.table.udf import udf
+from python_ai_flow import ExampleExecutor
+from typing import List
+import threading
+import pandas as pd
+import ai_flow as af
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+
+from streamz import Stream
+
+from flink_ai_flow.pyflink.user_define_executor import Executor, SourceExecutor, FlinkFunctionContext, SinkExecutor, \
+    TableEnvCreator
+from python_ai_flow import FunctionContext, Executor as PythonExecutor
+
+EXAMPLE_COLUMNS = ['sl', 'sw', 'pl', 'pw', 'type']
+
+
+class ReadTrainExample:
+    class SourceThread(threading.Thread):
+        def __init__(self, stream_uri):
+            super().__init__()
+            self.stream = Stream()
+            self.stream_uri = stream_uri
+
+        def run(self) -> None:
+            for i in range(0, 3):
+                train_data = pd.read_csv(self.stream_uri, header=0, names=EXAMPLE_COLUMNS)
+                y_train = train_data.pop(EXAMPLE_COLUMNS[4])
+                self.stream.emit((train_data.values, y_train.values))
+                print('send-{}-example'.format(i))
+                sleep(50)
+
+    class LoadTrainExample(ExampleExecutor):
+        def __init__(self):
+            self.thread = None
+
+        def setup(self, function_context: FunctionContext):
+            example_meta: ExampleMeta = function_context.node_spec.example_meta
+            self.thread = ReadTrainExample.SourceThread(example_meta.stream_uri)
+            self.thread.start()
+
+        def execute(self, function_context: FunctionContext, input_list: List) -> List:
+            return [self.thread.stream]
+
+
+class TrainModel(PythonExecutor):
+
+    def execute(self, function_context: FunctionContext, input_list: List) -> List:
+        def sink(df):
+            pass
+
+        def train(df):
+            x_train, y_train = df[0], df[1]
+            sk_model = KNeighborsClassifier(n_neighbors=5)
+            sk_model.fit(x_train, y_train)
+            model_meta = function_context.node_spec.output_model
+            model_path = get_file_dir(__file__) + '/saved_model'
+            if not os.path.exists(model_path):
+                os.makedirs(model_path)
+            model_timestamp = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime())
+            model_path = model_path + '/' + model_timestamp
+            dump(sk_model, model_path)
+            af.register_model_version(model=model_meta, model_path=model_path)
+            print('model update! {}'.format(model_meta.name))
+            print(af.get_latest_generated_model_version(model_name=model_meta.name).model_path)
+            print(af.get_latest_generated_model_version(model_name=model_meta.name).version)
+            return df
+
+        data: Stream = input_list[0]
+        data.map(train).sink(sink)
+        return []
+
+
+class TestExampleReader(Executor):
+    def execute(self, function_context: FunctionContext, input_list: List) -> List:
+        train_data = pd.read_csv(function_context.node_spec.example_meta.stream_uri, header=0, names=EXAMPLE_COLUMNS)
+        y_train = train_data.pop(EXAMPLE_COLUMNS[4])
+        return [[train_data, y_train]]
+
+
+class EvaluateModel(PythonExecutor):
+    def execute(self, function_context: FunctionContext, input_list: List) -> List:
+        print('evaluate' + generate_time_str())
+        model_name = function_context.node_spec.model.name
+        model_meta = af.get_latest_generated_model_version(model_name)
+        model_path = model_meta.model_path
+        
+        evaluate_example = input_list[0][0]
+        y_evaluate = input_list[0][1]
+        clf = load(model_path)
+        scores = cross_val_score(clf, evaluate_example, y_evaluate, cv=5)
+        evaluate_artifact = af.get_artifact_by_name('evaluate_artifact').stream_uri
+        print(scores)
+        
+        with open(evaluate_artifact, 'a') as f:
+            f.write('model version: {} scores: {}\n'.format(model_meta.version, scores))
+        return []
+
+
+class ValidateModel(PythonExecutor):
+
+    def execute(self, function_context: FunctionContext, input_list: List) -> List:
+        current_model_meta: ModelMeta = function_context.node_spec.model
+        deployed_model_version = af.get_deployed_model_version(model_name=current_model_meta.name)
+
+        new_model_meta = af.get_latest_generated_model_version(function_context.node_spec.model.name)
+        print(new_model_meta.version)
+        print(new_model_meta.model_path)
+        if deployed_model_version is None:
+            print("New Best Model Version {}".format(new_model_meta.version))
+            af.update_model_version(model_name=current_model_meta.name,
+                                    model_version=new_model_meta.version,
+                                    current_stage=ModelVersionStage.VALIDATED)
+            af.update_model_version(model_name=current_model_meta.name,
+                                    model_version=new_model_meta.version,
+                                    current_stage=ModelVersionStage.DEPLOYED)
+        else:
+            x_validate = input_list[0][0]
+            y_validate = input_list[0][1]
+            knn = load(new_model_meta.model_path)
+            scores = knn.score(x_validate, y_validate)
+            deployed_knn = load(deployed_model_version.model_path)
+            deployed_scores = deployed_knn.score(x_validate, y_validate)
+            stream_uri = af.get_artifact_by_name('validate_artifact').stream_uri
+            with open(stream_uri, 'a') as f:
+                f.write(
+                    'deployed model version: {} scores: {}\n'.format(deployed_model_version.version, deployed_scores))
+                f.write('generated model version: {} scores: {}\n'.format(new_model_meta.version, scores))
+            print("New Best Model Version {} Old {}".format(new_model_meta.version, deployed_model_version.version))
+            if scores >= deployed_scores:
+                af.update_model_version(model_name=current_model_meta.name,
+                                        model_version=deployed_model_version.version,
+                                        current_stage=ModelVersionStage.DEPRECATED)
+                af.update_model_version(model_name=current_model_meta.name,
+                                        model_version=new_model_meta.version,
+                                        current_stage=ModelVersionStage.VALIDATED)
+                af.update_model_version(model_name=current_model_meta.name,
+                                        model_version=new_model_meta.version,
+                                        current_stage=ModelVersionStage.DEPLOYED)
+        return []
+
+
+class StreamTableEnvCreator(TableEnvCreator):
+
+    def create_table_env(self):
+        stream_env = StreamExecutionEnvironment.get_execution_environment()
+        stream_env.set_parallelism(1)
+        t_env = StreamTableEnvironment.create(
+            stream_env,
+            environment_settings=EnvironmentSettings.new_instance().use_blink_planner().build())
+        statement_set = t_env.create_statement_set()
+        t_env.get_config().get_configuration().set_boolean("python.fn-execution.memory.managed", True)
+        return stream_env, t_env, statement_set
+
+
+class Source(SourceExecutor):
+    def execute(self, function_context: FlinkFunctionContext) -> Table:
+        print("### {} setup done2 for {}".format(self.__class__.__name__, "sads"))
+        example_meta: ExampleMeta = function_context.get_example_meta()
+        t_env = function_context.get_table_env()
+        print(example_meta.batch_uri)
+        t_env.connect(FileSystem().path(example_meta.batch_uri)) \
+            .with_format(OldCsv()
+                         .ignore_first_line()
+                         .field(EXAMPLE_COLUMNS[0], DataTypes.FLOAT())
+                         .field(EXAMPLE_COLUMNS[1], DataTypes.FLOAT())
+                         .field(EXAMPLE_COLUMNS[2], DataTypes.FLOAT())
+                         .field(EXAMPLE_COLUMNS[3], DataTypes.FLOAT())) \
+            .with_schema(Schema()
+                         .field(EXAMPLE_COLUMNS[0], DataTypes.FLOAT())
+                         .field(EXAMPLE_COLUMNS[1], DataTypes.FLOAT())
+                         .field(EXAMPLE_COLUMNS[2], DataTypes.FLOAT())
+                         .field(EXAMPLE_COLUMNS[3], DataTypes.FLOAT())) \
+            .create_temporary_table('mySource')
+        return t_env.from_path('mySource')
+
+
+class Transformer(Executor):
+    def __init__(self):
+        super().__init__()
+        self.model_name = None
+
+    def setup(self, function_context: FunctionContext):
+        self.model_name = function_context.node_spec.model.name
+
+    def execute(self, function_context: FlinkFunctionContext, input_list: List[Table]) -> List[Table]:
+        model_meta = af.get_deployed_model_version(self.model_name)
+        model_path = model_meta.model_path
+        print(model_path)
+        clf = load(model_path)
+
+        class Predict(ScalarFunction):
+            def eval(self, sl, sw, pl, pw):
+                records = [[sl, sw, pl, pw]]
+                df = pd.DataFrame.from_records(records, columns=['sl', 'sw', 'pl', 'pw'])
+                return clf.predict(df)[0]
+
+        udf_func = udf(Predict(), input_types=[DataTypes.FLOAT()] * 4, result_type=DataTypes.FLOAT())
+        function_context.get_table_env().register_function('udf', udf_func)
+        return [input_list[0].select("udf(sl,sw,pl,pw)")]
+
+
+class Sink(SinkExecutor):
+
+    def execute(self, function_context: FlinkFunctionContext, input_table: Table) -> None:
+        print("### {} setup done".format(self.__class__.__name__))
+        table_env = function_context.get_table_env()
+        table_env.register_table_sink("write_example", CsvTableSink(
+            ['a'],
+            [DataTypes.FLOAT()],
+            function_context.get_example_meta().batch_uri,
+            write_mode=WriteMode.OVERWRITE
+        ))
+        function_context.statement_set.add_insert("write_example", input_table)
+        print("### {} table_env execute done {}".format(self.__class__.__name__,
+                                                        function_context.get_example_meta().batch_uri))
