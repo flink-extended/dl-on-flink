079b3027b361e44e6e7b5f9770fe7d932bd46916
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-07-20 14:07:19 +0800

    [Flink AI Flow] [Test] Add Java Client Unit Test

---
+++ flink-ai-flow/ai_flow/java/client/src/test/java/com/aiflow/client/SingleJUnitTestRunner.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.aiflow.client;
+
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectMethod;
+
+public class SingleJUnitTestRunner {
+    public static void main(String[] args) throws ClassNotFoundException {
+        String[] classAndMethod = args[0].split("#");
+
+        System.out.println(Class.forName(classAndMethod[0]));
+        System.out.println(classAndMethod[1]);
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(
+                        selectMethod(Class.forName(classAndMethod[0]),
+                                classAndMethod[1])
+                )
+                .build();
+        Launcher launcher = LauncherFactory.create();
+
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        launcher.registerTestExecutionListeners(listener);
+
+        launcher.execute(request);
+
+        TestExecutionSummary summary = listener.getSummary();
+        for (TestExecutionSummary.Failure failure: summary.getFailures()) {
+            System.out.println(failure.getTestIdentifier());
+            System.out.println(failure.getException());
+            failure.getException().printStackTrace();
+        }
+        System.exit(summary.getFailures().size() == 0 ? 0 : 1);
+
+/*
+        Request request = Request.method(Class.forName(classAndMethod[0]),
+                classAndMethod[1]);
+
+        Result result = new JUnitCore().run(request);
+        for (Failure f : result.getFailures()) {
+            System.out.println(f.getDescription());
+            System.out.println(f.get.getTrimmedTrace());
+            System.out.println(f.getTrace());
+            System.out.println(f.getException());
+        }
+
+        System.exit(result.wasSuccessful() ? 0 : 1);*/
+    }
+}
