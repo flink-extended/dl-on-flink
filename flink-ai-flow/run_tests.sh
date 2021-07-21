f5f70b60a2a525f568d7aa096c62a53d5a358f6f
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-07-21 15:22:12 +0800

    [Flink AI Flow][Test] Use wildcard to search uber jar and add tests to ci

--- flink-ai-flow/run_tests.sh
+++ flink-ai-flow/run_tests.sh
@@ -69,6 +69,7 @@ run_tests 'ai_flow/test/endpoint/server/'
 run_test_class 'ai_flow/test/endpoint/' 'test_client.TestAIFlowClientSqlite'
 run_test_class 'ai_flow/test/endpoint/' 'test_mysql_client.TestAIFlowClientMySQL'
 run_test_class 'ai_flow/test/endpoint/' 'test_high_availability.TestHighAvailableAIFlowServer'
+run_test_class 'ai_flow/test/endpoint/' 'test_java_client.JavaAIFlowClientTest'
 
 run_tests 'ai_flow_plugins/tests/blob_manager_plugins'
 run_tests 'ai_flow_plugins/tests/job_plugins'
