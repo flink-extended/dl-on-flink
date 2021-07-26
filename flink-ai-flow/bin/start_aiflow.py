c4df74355220540a1d3290d801413291ab56629e
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-07-26 11:47:39 +0800

    [Flink AI Flow] Add two parameters to start_aiflow.py

--- flink-ai-flow/bin/start_aiflow.py
+++ flink-ai-flow/bin/start_aiflow.py
@@ -17,14 +17,20 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+import argparse
 import os
 import textwrap
 import ai_flow
 from airflow.logging_config import configure_logging
 
 
-def create_default_sever_config(root_dir_path):
-    airflow_deploy_path = os.environ["AIRFLOW_DEPLOY_PATH"]
+def create_default_sever_config(root_dir_path, db_uri, airflow_deploy_path):
+    """
+    Generate default server config which use Apache Airflow as scheduler.
+    """
+    from ai_flow.store.db.db_util import extract_db_engine_from_uri
+
+    db_type = extract_db_engine_from_uri(db_uri)
     content = textwrap.dedent(f"""\
         # Config of master server
 
@@ -33,15 +39,16 @@ def create_default_sever_config(root_dir_path):
         server_port: 50051
 
         # uri of database backend of AIFlow server
-        db_uri: sqlite:///{root_dir_path}/aiflow.db
+        db_uri: {db_uri}
 
         # type of database backend in master
-        db_type: sql_lite
+        db_type: {db_type}
 
         # whether to start the scheduler service
         start_scheduler_service: True
 
-        # uri of notification service
+        # Whether to start default notification service, if not a custom URI should be set
+        start_default_notification: False
         notification_uri: localhost:50052
 
         # scheduler config
@@ -64,12 +71,24 @@ def start_master(master_yaml_path):
     return server_runner
 
 
+def _prepare_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--database-conn', type=str, default=None,
+                        help='Database connection info')
+    parser.add_argument('--airflow-deploy-path', type=str, default=None,
+                        help='The directory which airflow read dags')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
+    args = _prepare_args()
+    database_conn = args.database_conn
+    airflow_deploy_path = args.airflow_deploy_path
     if "AIRFLOW_HOME" in os.environ:
         root_dir = os.environ["AIRFLOW_HOME"]
     else:
         root_dir = os.environ["HOME"] + "/airflow"
     master_yaml = root_dir + "/master.yaml"
     if not os.path.exists(master_yaml):
-        master_yaml = create_default_sever_config(root_dir)
+        master_yaml = create_default_sever_config(root_dir, database_conn, airflow_deploy_path)
     start_master(master_yaml)
