bb7758b2914dcaf84d37390ed432551c05135566
Xuannan <suxuannan95@gmail.com>
2021-08-20 14:58:29 +0800

    [Airflow] EventBasedScheduler uses ContextExtractor to decide the dagrun to handle events (#481)
    * [Airflow] Add context extractor to DAG and SerializedDag
    
    * [Airflow] EventBasedScheduler use context extractor to filter dagruns to handle event

---
+++ flink-ai-flow/lib/airflow/tests/dags/fixed_context_extractor.py
@@ -0,0 +1,19 @@
+from ai_flow.api.context_extractor import ContextExtractor
+from typing import Optional, Text
+
+from airflow.events.context_extractor import EventContext, Broadcast, ContextList
+from notification_service.base_notification import BaseEvent
+
+
+class FixedContextExtractor(ContextExtractor):
+
+    def __init__(self, context):
+        self._context = context
+
+    def extract_context(self, event: BaseEvent) -> EventContext:
+        if event.key == 'broadcast':
+            return Broadcast()
+
+        context_list = ContextList()
+        context_list.add_context(self._context)
+        return context_list
