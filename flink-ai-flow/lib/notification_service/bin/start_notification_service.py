bd7a2b09096d1850b1095500edf36886def15924
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-08-25 15:16:47 +0800

    [Flink AI Flow] [NS] Set notification uri in start scripts

--- flink-ai-flow/lib/notification_service/bin/start_notification_service.py
+++ flink-ai-flow/lib/notification_service/bin/start_notification_service.py
@@ -28,7 +28,7 @@ from notification_service.event_storage import DbEventStorage
 def start_notification_service(port: int = 50052,
                                db_conn: str = None,
                                enable_ha: bool = False,
-                               server_uris: str = None,
+                               server_uri: str = None,
                                create_table_if_not_exists: bool = True):
     if db_conn:
         storage = DbEventStorage(db_conn, create_table_if_not_exists)
@@ -36,14 +36,15 @@ def start_notification_service(port: int = 50052,
         raise Exception('Failed to start notification service without database connection info.')
 
     if enable_ha:
-        if not server_uris:
-            raise Exception("When HA enabled, server_uris must be set.")
+        if not server_uri:
+            raise Exception("When HA enabled, server-uri must be set.")
+        port = server_uri.split(':')[1]
         ha_storage = DbHighAvailabilityStorage(db_conn=db_conn)
         ha_manager = SimpleNotificationServerHaManager()
         service = HighAvailableNotificationService(
             storage,
             ha_manager,
-            server_uris,
+            server_uri,
             ha_storage,
             5000)
         master = NotificationMaster(service=service,
@@ -58,13 +59,15 @@ def start_notification_service(port: int = 50052,
 def _prepare_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=50052,
-                        help='The port on which to run notification service， default is 50052')
+                        help='The port on which to run notification service，default is 50052. '
+                             'It would be ignored when server-uri is set in HA mode.')
     parser.add_argument('--database-conn', type=str, default=None,
                         help='Database connection info')
     parser.add_argument('--enable-ha', type=bool, default=False,
                         help='Whether to start a notification service with HA enabled, default is False')
-    parser.add_argument('--server-uris', type=str, default=None,
-                        help='When HA is enabled, multiple server URIs must be set with comma separated')
+    parser.add_argument('--server-uri', type=str, default=None,
+                        help='Need to be set when HA is enabled. '
+                             'It should be the URI of local notification server that can be reached from others')
     return parser.parse_args()
 
 
@@ -74,9 +77,9 @@ if __name__ == '__main__':
     ns_port = args.port
     database_conn = args.database_conn
     enable_ha = args.enable_ha
-    server_uris = args.server_uris
+    server_uri = args.server_uri
 
     start_notification_service(port=ns_port,
                                db_conn=database_conn,
                                enable_ha=enable_ha,
-                               server_uris=server_uris)
+                               server_uri=server_uri)
