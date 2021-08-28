501f7b6563469af12930272d0b412c066dd5e62b
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-08-28 22:39:19 +0800

    [Flink AI Flow] Remove server_ip configuration from aiflow_server.yaml

---
+++ flink-ai-flow/ai_flow/util/net_utils.py
@@ -0,0 +1,36 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import socket
+
+
+def get_ip_addr():
+    """
+    Get ip address of localhost by UDP socket.
+    :return: ip address of localhost
+    """
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    except OSError:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+        return ip
