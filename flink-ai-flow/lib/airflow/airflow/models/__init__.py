7d140a96d8dbd0cc8486a1bca51bf1ad7cb3379e
wuchaochen <wuchao_chen@qq.com>
2021-09-15 11:36:02 +0800

    [Flink AI Flow][Airflow] Fix the bug that the logs of the task instance are missing on display (#554)

--- flink-ai-flow/lib/airflow/airflow/models/__init__.py
+++ flink-ai-flow/lib/airflow/airflow/models/__init__.py
@@ -32,6 +32,7 @@ from airflow.models.skipmixin import SkipMixin
 from airflow.models.slamiss import SlaMiss
 from airflow.models.taskfail import TaskFail
 from airflow.models.taskinstance import TaskInstance, clear_task_instances
+from airflow.models.taskexecution import TaskExecution
 from airflow.models.taskreschedule import TaskReschedule
 from airflow.models.variable import Variable
 from airflow.models.xcom import XCOM_RETURN_KEY, XCom
