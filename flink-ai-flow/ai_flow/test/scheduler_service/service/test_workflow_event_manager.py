6ba61fb42c93d3b203f84be519e323ae7deec56e
Xuannan <suxuannan95@gmail.com>
2021-09-17 20:09:41 +0800

    [AIFlow] WorkflowEventManager restarts event processor (#561)

--- flink-ai-flow/ai_flow/test/scheduler_service/service/test_workflow_event_manager.py
+++ flink-ai-flow/ai_flow/test/scheduler_service/service/test_workflow_event_manager.py
@@ -14,7 +14,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import time
 import unittest
+from unittest import mock
+
+from notification_service.base_notification import BaseEvent
 
 from notification_service.master import NotificationMaster
 
@@ -23,7 +27,7 @@ from notification_service.event_storage import MemoryEventStorage
 from notification_service.service import NotificationService
 
 from ai_flow.scheduler_service.service.config import SchedulerServiceConfig
-from ai_flow.scheduler_service.service.workflow_event_manager import WorkflowEventManager
+from ai_flow.scheduler_service.service.workflow_event_manager import WorkflowEventManager, WorkflowEventWatcher
 
 
 class TestWorkflowEventManager(unittest.TestCase):
@@ -45,3 +49,46 @@ class TestWorkflowEventManager(unittest.TestCase):
         workflow_event_manager = WorkflowEventManager('localhost:50051', 'sqlite:///:memory:', scheduler_service_config)
         workflow_event_manager.start()
         workflow_event_manager.stop()
+
+    def test_workflow_event_manager_restart_event_processor(self):
+        scheduler_service_config = SchedulerServiceConfig(self.config)
+        workflow_event_manager = \
+            WorkflowEventManager('localhost:50051', 'sqlite:///:memory:', scheduler_service_config)
+        with mock.patch.object(workflow_event_manager, '_create_event_processor_process',
+                               wraps=workflow_event_manager._create_event_processor_process) as create_process:
+            previous_process = workflow_event_manager.event_processor_process
+            workflow_event_manager.start()
+            try:
+                # kill the process
+                previous_process.kill()
+                time.sleep(5)
+                create_process.assert_called_once()
+                self.assertNotEqual(previous_process, workflow_event_manager.event_processor_process)
+            finally:
+                workflow_event_manager.stop()
+
+    def test_notify_event_arrived(self):
+        scheduler_service_config = SchedulerServiceConfig(self.config)
+        workflow_event_manager = \
+            WorkflowEventManager('localhost:50051', 'sqlite:///:memory:', scheduler_service_config)
+        event = BaseEvent("k1", "v1")
+        with mock.patch.object(workflow_event_manager, 'conn') as mock_conn:
+            workflow_event_manager.notify_event_arrived(event)
+            mock_conn.send.assert_called_once_with(event)
+
+        with mock.patch.object(workflow_event_manager, 'conn') as mock_conn:
+            mock_conn.send.side_effect = [Exception("Boom!"), Exception("Boom!"), None]
+            workflow_event_manager.notify_event_arrived(event)
+            self.assertEqual(3, mock_conn.send.call_count)
+
+
+class TestWorkflowEventWatcher(unittest.TestCase):
+    def test_process(self):
+        workflow_event_manager = mock.MagicMock()
+        watcher = WorkflowEventWatcher(workflow_event_manager)
+        e1 = BaseEvent("k1", "v1")
+        e2 = BaseEvent("k2", "v2")
+        e3 = BaseEvent("k3", "v3")
+        events = [e1, e2, e3]
+        watcher.process(events)
+        workflow_event_manager.notify_event_arrived.assert_has_calls([mock.call(e1), mock.call(e2), mock.call(e3)])
