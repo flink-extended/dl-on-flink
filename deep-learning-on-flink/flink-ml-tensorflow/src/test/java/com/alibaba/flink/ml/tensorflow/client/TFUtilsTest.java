c71a14370eca34f22d4335a54484670585b07504
sxnan <suxuannan95@gmail.com>
2021-11-17 18:10:56 +0800

    Update flink-ml-tensorflow to flink 1.14

--- deep-learning-on-flink/flink-ml-tensorflow/src/test/java/com/alibaba/flink/ml/tensorflow/client/TFUtilsTest.java
+++ deep-learning-on-flink/flink-ml-tensorflow/src/test/java/com/alibaba/flink/ml/tensorflow/client/TFUtilsTest.java
@@ -21,7 +21,6 @@ package com.alibaba.flink.ml.tensorflow.client;
 import com.alibaba.flink.ml.cluster.MLConfig;
 import com.alibaba.flink.ml.operator.coding.RowCSVCoding;
 import com.alibaba.flink.ml.operator.source.DebugRowSource;
-import com.alibaba.flink.ml.operator.table.descriptor.TableDebugRowDescriptor;
 import com.alibaba.flink.ml.operator.util.TypeUtil;
 import com.alibaba.flink.ml.tensorflow.hooks.DebugHook;
 import com.alibaba.flink.ml.tensorflow.util.TFConstants;
@@ -33,176 +32,180 @@ import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.StatementSet;
 import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableDescriptor;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.table.descriptors.Schema;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-
 public class TFUtilsTest {
-	private static TestingServer server;
-	private static final String pythonPath = TestUtil.getProjectRootPath() + "/flink-ml-tensorflow/src/test/python/";
-	private static final String add = pythonPath + "add.py";
-	private static final String workerZeroFinishScript = pythonPath + "worker_0_finish.py";
-	private static final String addTBScript = pythonPath + "add_withtb.py";
-	private static final String inputOutputScript = pythonPath + "input_output.py";
-	private static final String tensorboardScript = pythonPath + "tensorboard.py";
-	private static final String ckptDir = TestUtil.getProjectRootPath() + "/flink-ml-tensorflow/target/tmp/add_withtb/";
+    private static TestingServer server;
+    private static final String pythonPath = TestUtil.getProjectRootPath() + "/flink-ml-tensorflow/src/test/python/";
+    private static final String add = pythonPath + "add.py";
+    private static final String workerZeroFinishScript = pythonPath + "worker_0_finish.py";
+    private static final String addTBScript = pythonPath + "add_withtb.py";
+    private static final String inputOutputScript = pythonPath + "input_output.py";
+    private static final String tensorboardScript = pythonPath + "tensorboard.py";
+    private static final String ckptDir = TestUtil.getProjectRootPath() + "/flink-ml-tensorflow/target/tmp/add_withtb/";
 
-	@Before
-	public void setUp() throws Exception {
-		server = new TestingServer(2181, true);
-	}
+    @Before
+    public void setUp() throws Exception {
+        server = new TestingServer(2181, true);
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		server.stop();
-	}
+    @After
+    public void tearDown() throws Exception {
+        server.stop();
+    }
 
-	@Test
-	public void addTrainStream() throws Exception {
-		System.out.println(SysUtil._FUNC_());
-		StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+    @Test
+    public void addTrainStream() throws Exception {
+        System.out.println(SysUtil._FUNC_());
+        StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		TFConfig config = new TFConfig(2, 1, null, add, "map_func", null);
-		TFUtils.train(streamEnv, null, config);
+        TFConfig config = new TFConfig(2, 1, null, add, "map_func", null);
+        TFUtils.train(streamEnv, null, config);
 
-		JobExecutionResult result = streamEnv.execute();
-		System.out.println(result.getNetRuntime());
-	}
+        JobExecutionResult result = streamEnv.execute();
+        System.out.println(result.getNetRuntime());
+    }
 
-	@Test
-	public void addTrainTable() throws Exception {
-		System.out.println(SysUtil._FUNC_());
-		StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-		TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
-		StatementSet statementSet = tableEnv.createStatementSet();
+    @Test
+    public void addTrainTable() throws Exception {
+        System.out.println(SysUtil._FUNC_());
+        StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
+        StatementSet statementSet = tableEnv.createStatementSet();
 
-		TFConfig config = new TFConfig(2, 1, null, add, "map_func", null);
-		TFUtils.train(streamEnv, tableEnv, statementSet, null, config, null);
+        TFConfig config = new TFConfig(2, 1, null, add, "map_func", null);
+        TFUtils.train(streamEnv, tableEnv, statementSet, null, config, null);
 
-		execTableJobCustom(config.getMlConfig(), streamEnv, tableEnv, statementSet);
-	}
+        execTableJobCustom(config.getMlConfig(), streamEnv, tableEnv, statementSet);
+    }
 
-	@Test
-	public void addTrainChiefAloneStream() throws Exception {
-		System.out.println(SysUtil._FUNC_());
-		StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+    @Test
+    public void addTrainChiefAloneStream() throws Exception {
+        System.out.println(SysUtil._FUNC_());
+        StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		TFConfig config = new TFConfig(2, 1, null, add, "map_func", null);
-		config.addProperty(TFConstants.TF_IS_CHIEF_ALONE, "true");
+        TFConfig config = new TFConfig(2, 1, null, add, "map_func", null);
+        config.addProperty(TFConstants.TF_IS_CHIEF_ALONE, "true");
 
-		TFUtils.train(streamEnv, null, config);
+        TFUtils.train(streamEnv, null, config);
 
-		JobExecutionResult result = streamEnv.execute();
-		System.out.println(result.getNetRuntime());
-	}
+        JobExecutionResult result = streamEnv.execute();
+        System.out.println(result.getNetRuntime());
+    }
 
-	@Test
-	public void addTrainChiefAloneTable() throws Exception {
-		System.out.println(SysUtil._FUNC_());
-		StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-		TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
-		StatementSet statementSet = tableEnv.createStatementSet();
+    @Test
+    public void addTrainChiefAloneTable() throws Exception {
+        System.out.println(SysUtil._FUNC_());
+        StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
+        StatementSet statementSet = tableEnv.createStatementSet();
 
-		TFConfig config = new TFConfig(2, 1, null, add, "map_func", null);
-		config.addProperty(TFConstants.TF_IS_CHIEF_ALONE, "true");
-		TFUtils.train(streamEnv, tableEnv, statementSet, null, config, null);
+        TFConfig config = new TFConfig(2, 1, null, add, "map_func", null);
+        config.addProperty(TFConstants.TF_IS_CHIEF_ALONE, "true");
+        TFUtils.train(streamEnv, tableEnv, statementSet, null, config, null);
 
-		execTableJobCustom(config.getMlConfig(), streamEnv, tableEnv, statementSet);
-	}
+        execTableJobCustom(config.getMlConfig(), streamEnv, tableEnv, statementSet);
+    }
 
-	@Test
-	public void inputOutputTable() throws Exception {
-		System.out.println(SysUtil._FUNC_());
-		StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+    @Test
+    public void inputOutputTable() throws Exception {
+        System.out.println(SysUtil._FUNC_());
+        StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		TFConfig config = new TFConfig(2, 1, null, inputOutputScript, "map_func", null);
-		config.getProperties().put(MLConstants.ENCODING_CLASS, RowCSVCoding.class.getCanonicalName());
-		config.getProperties().put(MLConstants.DECODING_CLASS, RowCSVCoding.class.getCanonicalName());
-		StringBuilder inputSb = new StringBuilder();
+        TFConfig config = new TFConfig(2, 1, null, inputOutputScript, "map_func", null);
+        config.getProperties().put(MLConstants.ENCODING_CLASS, RowCSVCoding.class.getCanonicalName());
+        config.getProperties().put(MLConstants.DECODING_CLASS, RowCSVCoding.class.getCanonicalName());
+        StringBuilder inputSb = new StringBuilder();
 
-		inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.INT_32.name()).append(",");
-		inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.INT_64.name()).append(",");
-		inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.FLOAT_32.name()).append(",");
-		inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.FLOAT_64.name()).append(",");
-		inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.STRING.name());
+        inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.INT_32.name()).append(",");
+        inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.INT_64.name()).append(",");
+        inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.FLOAT_32.name()).append(",");
+        inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.FLOAT_64.name()).append(",");
+        inputSb.append(com.alibaba.flink.ml.operator.util.DataTypes.STRING.name());
 
-		config.getProperties().put(RowCSVCoding.ENCODE_TYPES, inputSb.toString());
-		config.getProperties().put(RowCSVCoding.DECODE_TYPES, inputSb.toString());
-		TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
-		StatementSet statementSet = tableEnv.createStatementSet();
+        config.getProperties().put(RowCSVCoding.ENCODE_TYPES, inputSb.toString());
+        config.getProperties().put(RowCSVCoding.DECODE_TYPES, inputSb.toString());
+        TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
+        StatementSet statementSet = tableEnv.createStatementSet();
 
-		tableEnv.connect(new TableDebugRowDescriptor())
-				.withSchema(new Schema().schema(TypeUtil.rowTypeInfoToSchema(DebugRowSource.typeInfo)))
-				.createTemporaryTable("debug_source");
-		Table input = tableEnv.scan("debug_source");
-		tableEnv.connect(new TableDebugRowDescriptor())
-				.withSchema(new Schema().schema(TypeUtil.rowTypeInfoToSchema(DebugRowSource.typeInfo)))
-				.createTemporaryTable("table_row_sink");
-		Table table = TFUtils.train(streamEnv, tableEnv, statementSet, input, config,
-				TypeUtil.rowTypeInfoToSchema(DebugRowSource.typeInfo));
-		statementSet.addInsert("table_row_sink", table);
-		execTableJobCustom(config.getMlConfig(), streamEnv, tableEnv, statementSet);
-	}
+        tableEnv.createTemporaryTable("debug_source",
+                TableDescriptor
+                        .forConnector("TableDebug")
+                        .schema(TypeUtil.rowTypeInfoToSchema(DebugRowSource.typeInfo))
+                        .build());
 
-	@Test
-	public void testTensorBoard() throws Exception {
-		System.out.println(SysUtil._FUNC_());
-		StreamExecutionEnvironment flinkEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        Table input = tableEnv.scan("debug_source");
 
-		TFConfig config = new TFConfig(2, 1, null, addTBScript, "map_func", null);
-		config.getProperties().put(MLConstants.FLINK_HOOK_CLASSNAMES, DebugHook.class.getCanonicalName());
-		config.addProperty(MLConstants.CHECKPOINT_DIR, ckptDir + String.valueOf(System.currentTimeMillis()));
-		TFUtils.train(flinkEnv, null, config);
+        tableEnv.createTemporaryTable("table_row_sink",
+                TableDescriptor
+                        .forConnector("TableDebug")
+                        .schema(TypeUtil.rowTypeInfoToSchema(DebugRowSource.typeInfo))
+                        .build());
 
-		TFConfig tbConfig = config.deepCopy();
-		String[] scripts = { tensorboardScript };
-		tbConfig.setPythonFiles(scripts);
-		TFUtils.startTensorBoard(flinkEnv, tbConfig);
+        Table table = TFUtils.train(streamEnv, tableEnv, statementSet, input, config,
+                TypeUtil.rowTypeInfoToSchema(DebugRowSource.typeInfo));
+        statementSet.addInsert("table_row_sink", table);
+        execTableJobCustom(config.getMlConfig(), streamEnv, tableEnv, statementSet);
+    }
 
-		JobExecutionResult result = flinkEnv.execute();
-	}
+    @Test
+    public void testTensorBoard() throws Exception {
+        System.out.println(SysUtil._FUNC_());
+        StreamExecutionEnvironment flinkEnv = StreamExecutionEnvironment.getExecutionEnvironment();
 
-	@Test
-	public void testTensorBoardTable() throws Exception {
-		System.out.println(SysUtil._FUNC_());
-		StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-		TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
-		StatementSet statementSet = tableEnv.createStatementSet();
+        TFConfig config = new TFConfig(2, 1, null, addTBScript, "map_func", null);
+        config.getProperties().put(MLConstants.FLINK_HOOK_CLASSNAMES, DebugHook.class.getCanonicalName());
+        config.addProperty(MLConstants.CHECKPOINT_DIR, ckptDir + String.valueOf(System.currentTimeMillis()));
+        TFUtils.train(flinkEnv, null, config);
 
-		TFConfig config = new TFConfig(2, 1, null, addTBScript, "map_func", null);
-		config.getProperties().put(MLConstants.FLINK_HOOK_CLASSNAMES, DebugHook.class.getCanonicalName());
-		config.addProperty(MLConstants.CHECKPOINT_DIR, ckptDir + String.valueOf(System.currentTimeMillis()));
-		TFUtils.train(streamEnv, tableEnv, statementSet, null, config, null);
+        TFConfig tbConfig = config.deepCopy();
+        String[] scripts = {tensorboardScript};
+        tbConfig.setPythonFiles(scripts);
+        TFUtils.startTensorBoard(flinkEnv, tbConfig);
 
-		TFConfig tbConfig = config.deepCopy();
-		String[] scripts = { tensorboardScript };
-		tbConfig.setPythonFiles(scripts);
-		TFUtils.startTensorBoard(streamEnv, tableEnv, statementSet, tbConfig);
+        JobExecutionResult result = flinkEnv.execute();
+    }
 
-		statementSet.execute().getJobClient().get()
-				.getJobExecutionResult().get();
-	}
+    @Test
+    public void testTensorBoardTable() throws Exception {
+        System.out.println(SysUtil._FUNC_());
+        StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
+        StatementSet statementSet = tableEnv.createStatementSet();
 
-	@Test
-	public void testWorkerZeroFinish() throws Exception {
-		System.out.println(SysUtil._FUNC_());
-		StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-		TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
-		StatementSet statementSet = tableEnv.createStatementSet();
-		TFConfig config = new TFConfig(3, 2, null, workerZeroFinishScript, "map_func", null);
-		TFUtils.train(streamEnv, tableEnv, statementSet, null, config, null);
-		execTableJobCustom(config.getMlConfig(), streamEnv, tableEnv, statementSet);
-	}
+        TFConfig config = new TFConfig(2, 1, null, addTBScript, "map_func", null);
+        config.getProperties().put(MLConstants.FLINK_HOOK_CLASSNAMES, DebugHook.class.getCanonicalName());
+        config.addProperty(MLConstants.CHECKPOINT_DIR, ckptDir + String.valueOf(System.currentTimeMillis()));
+        TFUtils.train(streamEnv, tableEnv, statementSet, null, config, null);
 
-	public static void execTableJobCustom(MLConfig mlConfig, StreamExecutionEnvironment streamEnv,
-										  TableEnvironment tableEnv, StatementSet statementSet) throws Exception {
-		statementSet.execute().getJobClient().get()
-				.getJobExecutionResult().get();
-	}
+        TFConfig tbConfig = config.deepCopy();
+        String[] scripts = {tensorboardScript};
+        tbConfig.setPythonFiles(scripts);
+        TFUtils.startTensorBoard(streamEnv, tableEnv, statementSet, tbConfig);
+
+        statementSet.execute().getJobClient().get()
+                .getJobExecutionResult().get();
+    }
+
+    @Test
+    public void testWorkerZeroFinish() throws Exception {
+        System.out.println(SysUtil._FUNC_());
+        StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        TableEnvironment tableEnv = StreamTableEnvironment.create(streamEnv);
+        StatementSet statementSet = tableEnv.createStatementSet();
+        TFConfig config = new TFConfig(3, 2, null, workerZeroFinishScript, "map_func", null);
+        TFUtils.train(streamEnv, tableEnv, statementSet, null, config, null);
+        execTableJobCustom(config.getMlConfig(), streamEnv, tableEnv, statementSet);
+    }
+
+    public static void execTableJobCustom(MLConfig mlConfig, StreamExecutionEnvironment streamEnv,
+                                          TableEnvironment tableEnv, StatementSet statementSet) throws Exception {
+        statementSet.execute().getJobClient().get()
+                .getJobExecutionResult().get();
+    }
 }
