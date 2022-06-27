fa0bfd6931c86f14c64e02c02f15c8c7aca9795e
sxnan <suxuannan95@gmail.com>
2022-06-27 14:25:53 +0800

    Bump version to 0.6.0-SNAPSHOT

--- dl-on-flink-framework/src/test/java/org/flinkextended/flink/ml/util/SysUtilTest.java
+++ dl-on-flink-framework/src/test/java/org/flinkextended/flink/ml/util/SysUtilTest.java
@@ -24,6 +24,6 @@ public class SysUtilTest {
 
     @Test
     public void getProjectVersion() {
-        Assert.assertEquals("0.5.0-SNAPSHOT", SysUtil.getProjectVersion());
+        Assert.assertEquals("0.6.0-SNAPSHOT", SysUtil.getProjectVersion());
     }
 }
